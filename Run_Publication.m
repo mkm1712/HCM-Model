af2b862fcb60bf05e29a300e6a0f0fb14f36662c
Ali Khalilimeybodi <mkm1712@gmail.com>
2023-09-26 17:13:12 -0700

    Add files via upload

--- Run_Publication.m
+++ Run_Publication.m
@@ -9,22 +9,17 @@ params=[];y0=[];y1=[];y2=[]; y3=[];
 [params,y0] = Model_Publication_loadParams();
 
 % Setting Run parameters
- dt=0.1; 
- eps_val=0.05;
- tspan = 200; 
-%  pp=[10, 951]; %Plot variables
+dt=0.01; 
+eps_val=0.05;
+tspan = 200; 
 
  % Run initial simulation
 [t1,y1]=St_ode(params,y0,eps_val,tspan,dt);
-% plot(t1,y1(:,pp),Color='b')
-% hold on
 
 % Run Main simulation
 params{1,1}(1,1)=1;
 [t2,y2]=St_ode(params,y1(end,:),eps_val,tspan,dt);
 y2=real(y2);
-% plot(t2+tspan,y2(:,pp),Color='r') 
-% hold on
 
 % Amplification of genes
 Beta=2;
@@ -32,30 +27,22 @@ params{3}(93:end)=0.5*(y2(end,93:end)./y1(end,93:end)).^(Beta);
 params{3}(params{3}<0.001)=0.001;
 params{3}(params{3}>1)=1;     
 [t3,y3]=St_ode(params,y2(end,:),eps_val,tspan,dt);
-% plot(t3+2*tspan,y3(:,pp),Color='g')
-% hold on
 
 %ATP low simulation
 params{3}(36)=0.8; % Applying ATP low
 [t4,y4]=St_ode(params,y3(end,:),eps_val,tspan,dt);
 
-
-
 Sig_treatment (:,nn)=real(y4(end,1:86))';
 Sig_control (:,nn)=real(y1(end,1:86))';
-Sig_mid (:,nn)=real(y3(end,1:86))';
 Genes_treatment(:,nn)=real(y4(end,93:end))';
 Genes_control (:,nn)=real(y1(end,93:end))';
-Genes_mid (:,nn)=real(y3(end,93:end))';
 HYP_C(nn)=real(y1(end,89));
 HYP_T(nn)=real(y4(end,89));
 APO_C(nn)=real(y1(end,92));
 APO_T(nn)=real(y4(end,92));
 end
 toc
-%%  
-%unzip Main-Run file and then load it
-load ('Main-Run')
+%%
 % Data preparation
 [~, TF1]=rmmissing(Genes_control,2);
 [~, TF2]=rmmissing(Genes_treatment,2);
@@ -65,11 +52,14 @@ Genes_treatment(:,TF1==1)=nan;
 Genes_treatment(:,TF2==1)=nan;
 Genes_control=rmmissing(Genes_control,2);
 Genes_treatment=rmmissing(Genes_treatment,2);
+
 % Data Imputation
 for ii=1:1078
 Genes_control(ii,Genes_control(ii,:)==0)=mean(Genes_control(ii,:));
 Genes_treatment(ii,Genes_treatment(ii,:)==0)=mean(Genes_treatment(ii,:));
 end
+%%
+load ('Run-Main-Paper') % Model Data used for Fig 2
 %%% FC Calculation
 BB1=Genes_treatment./Genes_control;
 BB1(BB1==inf)=nan;
